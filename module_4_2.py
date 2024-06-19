f7d259af04ae0911cddf0e8ad286619f9fd77bde
parovoz33 <parovoz33@MAIL.RU>
2024-06-19 18:36:49 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -2,11 +2,11 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
 
-    # Вызов inner_function внутри test_function
+    
     inner_function()
 
 
-# Вызов test_function, который в свою очередь вызывает inner_function
+
 test_function()
 
 
